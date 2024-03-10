dc6613b81e954b455c9d28cc97e599d3a771e76d
MikeVazovski1104 <gamer.36076@yandex.ru>
2024-03-10 22:04:54 +0500

    Your commit message

--- homecats/cats/models.py
+++ homecats/cats/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+
 class Guest(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
@@ -6,16 +7,20 @@ class Guest(models.Model):
     def __str__(self):
         return f"first_name={self.first_name}\n last_name={self.last_name}\n"
 
-class Pets(models.Model):
-    Guest = models.ForeignKey(Guest, on_delete = models.CASCADE)
-    name = models.CharField(max_length=30)
-    achievement = models.ManyToManyField(Achievements)
-    def __str__(self):
-        return f"name={self.name}\n achievements={self.achievements}"
-
 class Achievements(models.Model):
     ACHIEV = (
         ('СК', 'Супер кусь'),
         ('ПНШ', 'Прыжок на шкаф'),
     )
-    achiev = models.CharField(max_length=3, choices=ACHIEV)
+    name = models.CharField(max_length=3, choices=ACHIEV)
+
+    def __str__(self):
+        return self.get_name_display()
+
+class Pets(models.Model):
+    guest = models.ForeignKey(Guest, on_delete=models.CASCADE)
+    name = models.CharField(max_length=30)
+    achievements = models.ManyToManyField(Achievements)
+
+    def __str__(self):
+        return f"name={self.name}\n achievements={[achievement.name for achievement in self.achievements.all()]}"
